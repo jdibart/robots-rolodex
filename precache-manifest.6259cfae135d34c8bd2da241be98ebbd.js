e89d4e555383c8a5520192e39c089c203fdb6517
ribart Dibart <dibart@gmail.com>
2020-07-27 15:58:54 -0400

    Updates

---
+++ precache-manifest.6259cfae135d34c8bd2da241be98ebbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82edba18d0a69c2b577888bcd45722f7",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "a15351b8990ae5d68083",
+    "url": "/robots-rolodex/static/css/main.63c60de5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/robots-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15351b8990ae5d68083",
+    "url": "/robots-rolodex/static/js/main.4916a0f0.chunk.js"
+  },
+  {
+    "revision": "f249aee8820cb360f70a",
+    "url": "/robots-rolodex/static/js/runtime-main.aa66a150.js"
+  }
+]);
